69bc09a498c9b0bf40595d88890d6bd7c2f845ec
Naohide Sano <namachapanda2005@gmail.com>
2022-12-01 10:16:36 +0900

    update settings

---
+++ src/main/java/vavi/apps/treeView/node/package-info.java
@@ -0,0 +1,1 @@
+package vavi.apps.treeView.node;
---
+++ src/main/java/vavi/apps/treeView/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Provides TreeView classes.
+ * <p>
+ * TODO
+ *  * make "userObject" "java beans"
+ *  * tell node modification to user object via messaging (observer, observable?)
+ *  * pluggable menu
+ *  * create menus by extracting verbs from a bean
+ *  * multiple DnD
+ *  * right panel is "beans property editor"
+ *  * try PropertyResourceBundle
+ */
+package vavi.apps.treeView;
+
